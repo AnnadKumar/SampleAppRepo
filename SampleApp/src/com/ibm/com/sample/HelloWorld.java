9e6b333b681ad12b488cfb79e4ecb1f50d6d3480
IBM <IBM@9.79.238.23>
2018-02-18 16:27:43 +0530

    second changes

--- SampleApp/src/com/ibm/com/sample/HelloWorld.java
+++ SampleApp/src/com/ibm/com/sample/HelloWorld.java
@@ -3,6 +3,7 @@ package com.ibm.com.sample;
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!!!");
+		System.out.println("Hello Git!!");
 	}
 
 }
